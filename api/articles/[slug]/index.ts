d862f2eb0b4fb1f2b6eb51b09b2e7554a98e7a79
Sanjaychagantipati <sanjaychagantipati@gmail.com>
2026-08-13 11:27:24 +0530

    fix: resolve Vercel routing conflict by restructuring to index.ts folders

--- api/articles/[slug]/index.ts
+++ api/articles/[slug]/index.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { getArticleBySlug } from '../services/articleService.js';
+import { getArticleBySlug } from '../../services/articleService.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { slug } = req.query;
--- api/chat/messages/[id]/index.ts
+++ api/chat/messages/[id]/index.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { verifyToken } from '../../utils/auth.js';
-import { getMessages, markConversationAsRead } from '../../services/chatService.js';
+import { verifyToken } from '../../../utils/auth.js';
+import { getMessages, markConversationAsRead } from '../../../services/chatService.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const user = verifyToken(req);
